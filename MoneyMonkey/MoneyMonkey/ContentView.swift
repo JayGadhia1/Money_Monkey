34e6941a84632096df99be73a4c77de54c8333e9
Ishaan Virani <77121609+Ishaan0601@users.noreply.github.com>
2022-11-13 04:22:28 -0600

    Add files via upload

--- MoneyMonkey/MoneyMonkey/ContentView.swift
+++ MoneyMonkey/MoneyMonkey/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State var username: String = ""
     @State var password: String = ""
     @State var savingsAcct: String = ""
+    @State var tBills: String = ""
+    @State var stocks: String = ""
+    @State var realEstate: String = ""
     
     var user: String = "User"
     var pass: String = "password"
@@ -49,7 +52,7 @@ struct ContentView: View {
                     }
                     if authenticationDidSucceed {
                         Text("Login Successful!").foregroundColor(.green).padding(.top, 10)
-                        NavigationLink(destination: Homepage(savingsAcct: $savingsAcct), label: { Text("Click Here to Continue")})
+                        NavigationLink(destination: Homepage(savingsAcct: $savingsAcct, realEstate: $realEstate, tBills: $tBills, stocks: $stocks), label: { Text("Click Here to Continue")})
                     }
                     Button(action: {
                         if(self.username == user && self.password == pass){
@@ -108,56 +111,77 @@ struct PasswordSecureField : View {
 
 struct Homepage : View {
     @Binding var savingsAcct: String
+    @Binding var realEstate: String
+    @Binding var tBills: String
+    @Binding var stocks: String
+    
     var body: some View {
-            ZStack{
-                Color(.black).edgesIgnoringSafeArea(.all)
-                VStack{
+        ZStack{
+            Color(.black).edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("MoneyMonkey_25-removebg-preview")
+                Group{
                     Text("Input Current Savings Below:")
+                        .frame(width: 500, height: 45)
                         .foregroundColor(Color(.white))
-                        .font(.system(size: 26))
-                        .multilineTextAlignment(.center)
-                        .padding(.top, 40)
+                        .font(.system(size: 18))
+                        .multilineTextAlignment(.leading)
                     TextField("Input Savings Amount Here", text: $savingsAcct)
                         .frame(width: 300, height: 45)
                         .background(Color(.white))
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .font(Font.system(size: 16))
                         .multilineTextAlignment(.center)
-                        .cornerRadius(22).padding(.top, 20)
-                    Text("Input Current Real Estate Investments Below:")
-                        .foregroundColor(Color(red: 224/255, green: 99/255, blue: 212/255))
-                        .font(.system(size: 26))
-                        .multilineTextAlignment(.center)
-                        .padding(.top, 40)
-                    TextField("Input Real Estate Investment Amount Here", text: $savingsAcct)
+                        .cornerRadius(22).padding(.top, 10)
+                    Text("Input Current Real Estate Investment Amount \nBelow:")
+                        .frame(width: 500, height: 45)
+                        .foregroundColor(Color(.white))
+                        .font(.system(size: 18))
+                        .multilineTextAlignment(.leading)
+                        .padding(.top, 20)
+                    TextField("Input Real Estate Investment Amount Here", text: $realEstate)
                         .frame(width: 300, height: 45)
                         .background(Color(.white))
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .font(Font.system(size: 16))
                         .multilineTextAlignment(.center)
-                        .cornerRadius(22).padding(.top, 20)
-                    TextField("Input Treasury Bonds Investment Amount Here", text: $savingsAcct)
+                        .cornerRadius(22).padding(.top, 10)
+                    Text("Input Current Treasury Bonds Investment Amount \nBelow:")
+                        .frame(width: 500, height: 45)
+                        .foregroundColor(Color(.white))
+                        .font(.system(size: 18))
+                        .multilineTextAlignment(.leading)
+                        .padding(.top, 20)
+                    TextField("Input Treasury Bonds Investment Amount Here", text: $tBills)
                         .frame(width: 300, height: 45)
                         .background(Color(.white))
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .font(Font.system(size: 16))
                         .multilineTextAlignment(.center)
-                        .cornerRadius(22).padding(.top, 20)
-                    TextField("Input Certificate of Deposit Amount Here", text: $savingsAcct)
+                        .cornerRadius(22).padding(.top, 10)
+                    Text("Input Current Stock Investment Amount Below:")
+                        .frame(width: 500, height: 45)
+                        .foregroundColor(Color(.white))
+                        .font(.system(size: 18))
+                        .multilineTextAlignment(.leading)
+                        .padding(.top, 20)
+                    TextField("Input Stocks Investment Amount Here", text: $stocks)
                         .frame(width: 300, height: 45)
                         .background(Color(.white))
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .font(Font.system(size: 16))
                         .multilineTextAlignment(.center)
-                        .cornerRadius(22).padding(.top, 20)
-                    TextField("Input Stocks Investment Amount Here", text: $savingsAcct)
-                        .frame(width: 300, height: 45)
-                        .background(Color(.white))
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .font(Font.system(size: 16))
-                        .multilineTextAlignment(.center)
-                        .cornerRadius(22).padding(.top, 20)
+                        .cornerRadius(22).padding(.top, 10)
                 }
+                let values: [Double] = [(savingsAcct as NSString).doubleValue, (realEstate as NSString).doubleValue, (tBills as NSString).doubleValue, (stocks as NSString).doubleValue]
+                //var values = [Double]()
+                //values.append((savingsAcct as NSString).doubleValue)
+                //values.append((realEstate as NSString).doubleValue)
+                //values.append((tBills as NSString).doubleValue)
+                //values.append((stocks as NSString).doubleValue)
+                let names: [String] = ["Savings", "Real Estate", "Treasury Bills", "Stocks"]
+                NavigationLink(destination: PieChartView(values: values, names: names, formatter: {value in String(format: "$%.2f", value)}), label: { Text("Click Here to Continue")})
             }
+        }
     }
 }
