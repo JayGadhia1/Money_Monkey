4fd938ce6d6803cc8aa91e49a0c2517e3a32c809
Ishaan Virani <77121609+Ishaan0601@users.noreply.github.com>
2022-11-13 03:26:32 -0600

    Add files via upload

---
+++ MoneyMonkey/MoneyMonkeyApp.swift
@@ -0,0 +1,19 @@
+//
+//  MoneyMonkeyApp.swift
+//  MoneyMonkey
+//
+//  Created by Ishaan virani on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMonkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView().preferredColorScheme(.dark)
+            //SignUp()
+            
+        }
+    }
+}
